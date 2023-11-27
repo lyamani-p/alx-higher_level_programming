b71b60cf18642bf046984f61b40d15ad1c48db34
lyamani-p <lyamani.abderrahmane@gmail.com>
2023-11-27 03:04:07 -0800

    first step into Python

---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+str1 = "Holberton"
+str2 = "School"
+
+# Concatenate str1 and str2 with a space in between
+welcome_message = f"Welcome to {str1} {str2}!"
+
+# Print the welcome message
+print(welcome_message)
